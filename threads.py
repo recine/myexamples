39049c007ff6652c89de19e39ba13a5b8be76d35
John Recine <31930975+recine@users.noreply.github.com>
2019-11-08 10:25:12 -0500

    Update threads.py

--- threads.py
+++ threads.py
@@ -6,13 +6,13 @@ import os
 from subprocess import Popen, PIPE
 
 def sqlplusFunc2 (sqlstr):
-	sqlplus2 = Popen(['sqlplus', '-S', 'stage01/Xtransfer1@67.48.243.13/prism03p_svc.corp.chartercom.com'], stdout=PIPE, stdin=PIPE)
-#	sqlplus2 = Popen(['sqlplus', '-S', 'stage01/Xtransfer1'], stdout=PIPE, stdin=PIPE)
+	sqlplus2 = Popen(['sqlplus', '-S', 'place01/pword@99.99.999.99/name.name.name.com'], stdout=PIPE, stdin=PIPE)
+#	sqlplus2 = Popen(['sqlplus', '-S', 'place01/pword'], stdout=PIPE, stdin=PIPE)
 	sqlplus2.stdin.write(sqlstr)
 	out , err = sqlplus2.communicate()
 #	sqlplus2.stdin.write("exit")
 	holdout = out.replace('"', '')
-	holdout = holdout.replace('STAGE01.','')
+	holdout = holdout.replace('PLACE01.','')
 	print holdout
 	return;
 	
@@ -21,8 +21,8 @@ print "Current time *************  START ****************" + time.strftime("%X")
 pool = ThreadPool(3) 
 	  
 thesql = [
-		"set long 9000;\n set head off;\n select substr(dbms_metadata.get_ddl('TABLE','UPD_PRISM_TAB','STAGE01'),1,((instr(dbms_metadata.get_ddl('TABLE','UPD_PRISM_TAB','STAGE01'), 'SEGMENT')-1)))  from dual;",
-		"set long 9000;\n set head off;\n select substr(dbms_metadata.get_ddl('TABLE','PRISM_TAB','STAGE01'),1,((instr(dbms_metadata.get_ddl('TABLE','PRISM_TAB','STAGE01'), 'SEGMENT')-1)))  from dual;",
+		"set long 9000;\n set head off;\n select substr(dbms_metadata.get_ddl('TABLE','UPD_PRI_TAB','STAGE01'),1,((instr(dbms_metadata.get_ddl('TABLE','UPD_PRI_TAB','STAGE01'), 'SEGMENT')-1)))  from dual;",
+		"set long 9000;\n set head off;\n select substr(dbms_metadata.get_ddl('TABLE','PRI_TAB','STAGE01'),1,((instr(dbms_metadata.get_ddl('TABLE','PRI_TAB','STAGE01'), 'SEGMENT')-1)))  from dual;",
 		"set long 9000;\n set head off;\n select substr(dbms_metadata.get_ddl('TABLE','RESULT_SET','STAGE01'),1,((instr(dbms_metadata.get_ddl('TABLE','RESULT_SET','STAGE01'), 'SEGMENT')-1)))  from dual;"
 	  ]
 
